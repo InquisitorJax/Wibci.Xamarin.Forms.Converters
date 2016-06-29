bcfd77d690e907f3d6f407331aa1557023fc7576
Malcolm Jack <inquisitorjax@gmail.com>
2016-06-30 00:49:43 +0200

    Add EmptyStringToFillTextConverter + NullValue to cater for white space

--- NullValueToBooleanConverter.cs
+++ NullValueToBooleanConverter.cs
@@ -3,21 +3,25 @@ using Xamarin.Forms;
 
 namespace Wibci.Xamarin.Forms.Converters
 {
-	public class NullValueToBooleanConverter : IValueConverter
-	{
-		public object Convert (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-		{
-			bool retValue = value == null;
-			if (parameter != null)
-				retValue = !retValue;
-			return retValue;
-		}
-		public object ConvertBack (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-		{
-			throw new NotImplementedException ();
-		}
+    public class NullValueToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool retValue = value == null;
 
+            if (!retValue && value is string)
+            {
+                retValue = string.IsNullOrWhiteSpace(value.ToString());
+            }
 
-	}
+            if (parameter != null)
+                retValue = !retValue;
+            return retValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
-
