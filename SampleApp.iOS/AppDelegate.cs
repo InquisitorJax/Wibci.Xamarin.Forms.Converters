51467e92dd2dc38d2f97e3386d2fc6b659cd15a6
Malcolm Jack <inquisitorjax@gmail.com>
2016-11-14 13:09:48 +0200

    add iOS Sample App

---
+++ SampleApp.iOS/AppDelegate.cs
@@ -0,0 +1,27 @@
+﻿using Foundation;
+using UIKit;
+using Wibci.Xamarin.Forms.Converters;
+
+namespace SampleApp.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            //CrossValueConverters.Init();
+
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
