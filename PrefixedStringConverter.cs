9c35a40b7f5901407c7042cfeb324da73ea02f17
Malcolm Jack <inquisitorjax@gmail.com>
2017-07-12 23:22:05 +0200

    add PrefixedStringConverter

---
+++ PrefixedStringConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Wibci.Xamarin.Forms.Converters
+{
+    public class PrefixedStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string prefix = parameter as string;
+            string fullString = value as string;
+            if (string.IsNullOrEmpty(prefix))
+                return value;
+
+            string displayValue = fullString;
+            if (!string.IsNullOrEmpty(displayValue) && displayValue.StartsWith(prefix))
+            {
+                displayValue = displayValue.Substring(prefix.Length, displayValue.Length - prefix.Length);
+            }
+            return displayValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string partialString = value as string;
+            string prefix = parameter as string;
+
+            string fullString = prefix + partialString;
+
+            return fullString;
+        }
+    }
